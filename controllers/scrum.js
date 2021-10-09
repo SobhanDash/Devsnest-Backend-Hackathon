efe5d8e84d1a9daf72262fb89b53590d4381df91
Mihir Harshe <mihir.harshe12@gmail.com>
2021-10-09 14:20:14 +0530

    added basic routes for scrum

---
+++ controllers/scrum.js
@@ -0,0 +1,21 @@
+const Scrum = require('../models/scrum');
+const Team = require('../models/team');
+const User = require('../models/user');
+
+const addScrum = async (req, res) => {
+
+};
+
+const viewScrum = async (req, res) => {
+
+};
+
+const updateScrum = async(req, res) => {
+
+};
+
+const markAttendance = async(req, res) => {
+
+};
+
+module.exports = { addScrum, viewScrum, updateScrum, markAttendance };
