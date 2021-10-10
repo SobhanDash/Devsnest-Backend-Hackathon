ffa32e585cbdaab0fa89baeedecda40d5fb6ba6c
SobhanDash <sobhan7dash@gmail.com>
2021-10-11 00:43:33 +0530

    config changed

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 
 module.exports = {
-  sequelize_database: process.env.DATABASE,
-  sequelize_username: process.env.USER,
-  sequelize_password: process.env.PASSWORD,
-  sequelize_host: process.env.HOST,
+  sequelize_database: process.env.SEQUELIZE_DATABASE,
+  sequelize_username: process.env.SEQUELIZE_USERNAME,
+  sequelize_password: process.env.SEQUELIZE_PASSWORD,
+  sequelize_host: process.env.SEQUELIZE_HOST,
   app_port: process.env.PORT,
   SECRET: process.env.SECRET,
 };
