be33d9aaa74da9c986337d652c05f2d8421be34f
omkarg1417 <omkarg1417@gmail.com>
2021-10-10 15:10:12 +0530

    added some useful comments

--- routes/team.js
+++ routes/team.js
@@ -17,15 +17,19 @@ const {isUser, isTeam} = require('../utils/validate')
 // User.hasOne(Team);
 // Team.belongsTo(User);
 
-
+//// localhost:3000/api/team/update/delete-team
 router.delete('/update/delete-team',checkRole(["admin", "batch-leader"]), deleteTeam);
 
+// localhost:3000/api/team/update/create-team
 router.post('/update/create-team',checkRole(["admin", "batch-leader"]), createTeam);
 
+// localhost:3000/api/team/update/team-name
 router.put('/update/team-name',checkRole(["admin", "batch-leader"]), updateTeamName);
 
+//localhost:3000/api/team/update/renove-member
 router.put('/update/remove-member',checkRole(["admin", "batch-leader"]), removeMember);
 
+//localhost:3000/api/team/update/add-member
 router.put('/update/add-member',checkRole(["admin", "batch-leader"]), addMember);
 
 
