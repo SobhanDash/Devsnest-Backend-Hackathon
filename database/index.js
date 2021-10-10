c6ee8d35eae2dc39f58cbcd70b8de6d116448727
SobhanDash <sobhan7dash@gmail.com>
2021-10-11 00:17:55 +0530

    Env files updated

--- database/index.js
+++ database/index.js
@@ -3,18 +3,16 @@ const {
   sequelize_database,
   sequelize_username,
   sequelize_password,
-  sequelize_host,
-  sequelize_dialect,
 } = require("../config");
 
 const sequelize = new Sequelize(
-    sequelize_database,
-    sequelize_username,
-    sequelize_password,
-    {
-        host: sequelize_host,
-        dialect: sequelize_dialect
-    }
+  sequelize_database,
+  sequelize_username,
+  sequelize_password,
+  {
+    host: "localhost",
+    dialect: "postgres",
+  }
 );
 
 sequelize.sync();
